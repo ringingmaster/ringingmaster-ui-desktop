37256260b6907e46dab14a0d21b5f35d73ba6484
Stephen Lake <stephen.j.lake@gmail.com>
2014-12-22 10:07:46 +0000

    correct spelling

--- src/main/java/com/concurrentperformance/ringingmaster/fxui/desktop/notationeditor/NotationEditorDialog.java
+++ src/main/java/com/concurrentperformance/ringingmaster/fxui/desktop/notationeditor/NotationEditorDialog.java
@@ -159,7 +159,7 @@ public class NotationEditorDialog {
 			items.clear();
 			items.add(new StatusModel("name", notation.getNameIncludingNumberOfBells()));
 			items.add(new StatusModel("notation", notation.getNotationDisplayString(true)));
-			items.add(new StatusModel("notation type", notation.isFoldedPalindrome()?"asymmetric":"asymmetric"));
+			items.add(new StatusModel("notation type", notation.isFoldedPalindrome()?"symmetric":"asymmetric"));
 			items.add(new StatusModel("plain course", plainCourseProof.getAnalysis().isTrueTouch()?"true":"false"));
 			items.add(new StatusModel("changes in plain lead", Integer.toString(plainCourse.getLead(0).getRowCount())));
 			items.add(new StatusModel("changes in plain course", Integer.toString(plainCourse.getRowCount())));

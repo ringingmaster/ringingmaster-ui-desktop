ff10e4cd0635539e085ddf996c1c62febc1a0008
Stephen Lake <stephen.j.lake@gmail.com>
2016-03-29 09:10:48 +0200

    Start building the notation chooser factory

---
+++ src/main/java/com/concurrentperformance/ringingmaster/ui/desktop/notationchooser/NotationChooserResults.java
@@ -0,0 +1,24 @@
+package com.concurrentperformance.ringingmaster.ui.desktop.notationchooser;
+
+import com.concurrentperformance.ringingmaster.persist.generated.v1.LibraryNotationPersist;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * TODO Comments
+ *
+ * @author Lake
+ */
+public class NotationChooserResults {
+
+    private List<LibraryNotationPersist> notations = Collections.emptyList();
+
+    public List<LibraryNotationPersist> getNotations() {
+        return notations;
+    }
+
+    public void setNotations(List<LibraryNotationPersist> notations) {
+        this.notations = notations;
+    }
+}
